0169903ece9287cfbaf706f7b14d709578d35703
satotech-prog <72504810+satotech-prog@users.noreply.github.com>
2020-12-15 18:07:17 +0900

    index修正

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,5 +3,27 @@ class TweetsController < ApplicationController
   def index
     @tweets = Tweet.all
   end
+<<<<<<< Updated upstream
   
+=======
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    @tweet = Tweet.create(tweet_params)
+    if @tweet.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  
+  def tweet_params
+    params.require(:tweet).permit(:title, :exposition, :image)#.merge(user_id: current_user.id)
+  end
+>>>>>>> Stashed changes
 end
